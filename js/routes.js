7b8dcd538310ec2da57f182556a5d816c187159f
erez2021 <hershko2013@gmail.com>
2021-02-24 11:31:50 +0200

    homepage

--- js/routes.js
+++ js/routes.js
@@ -1,22 +1,25 @@
 import keepApp from './keep/keep-page.cmp.js'
+import homePage from './pages/home-page.cmp.js'
+// import email from './email/email-pages.cmp.js'
 
 const routes = [
-    // {
-    //     path: '/',
-    //     component: homePage,
-    // },
     {
-        path: '/keep',
+        path: '/',
+        component: homePage,
+    },
+    {
+        path: '/keep/keep-page',
         component: keepApp
     },
     // {
-    //     path: '/about',
-    //     component: about,
-    //     children: [
-    //         {
-    //             path: 'team',
-    //             component: aboutTeam
-    //         },
+    //     path: '/email/email-pages',
+    //     component: email,
+    // }
+
+            // {
+            //     path: '/miss-book',
+            //     component: missBook
+            // },
     //         {
     //             path: 'services',
     //             component: aboutServices
