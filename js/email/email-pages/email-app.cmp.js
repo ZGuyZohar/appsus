11bb529892ea26ba808996b9a3b4bbbe8a5d0076
Guy Zohar <zguyzohar@gmail.com>
2021-02-24 20:58:03 +0200

    add email features

--- js/email/email-pages/email-app.cmp.js
+++ js/email/email-pages/email-app.cmp.js
@@ -1,23 +1,26 @@
 import {emailService} from '../services/email-storage-service.js'
 import asideNav from '../email-cmps/aside-nav.cmp.js'
 import mailList from '../email-cmps/email-list.cmp.js'
+import { eventBus } from '../../services/event-bus-service.js'
 
 export default {
     template: `
     <section class="email">
-        <aside-nav />
-        <mail-list v-if="emails" @isOpen="updateMail" :emails="emails" />
+        <aside-nav @filter="setFilter" @sent="sendMail" />
+        <mail-list v-if="emails" @isOpen="updateMail" :emails="mailsToShow" />
     </section>
     `,
     data(){
         return {
             emails: [],
+            filterBy: null
         }
     },
     methods: {
         loadEmails(){
             emailService.query()
             .then(emails => this.emails = emails)
+            
         },
         updateMail(currMail){
             emailService.getIdxById(currMail.id)
@@ -26,11 +29,39 @@ export default {
                 return this.emails[idx];
             })
             .then((mail) => emailService.updateMail(mail))
+        },
+        sendMail(newMail){
+            emailService.save(newMail)
+            .then(this.loadEmails)
+        },
+        setFilter(filter){
+            this.filterBy = filter;
+            console.log('worked!', filter);
+        }
+    },
+    computed: {
+        mailsToShow(){
+            if(this.filterBy === 'inbox' || !this.filterBy) {
+                return this.emails.filter(mail => {
+                    return !mail.isSent
+                })
+            }
+            if(this.filterBy === 'sent'){
+                return this.emails.filter(mail => {
+                    return mail.isSent
+                })
+            }
         }
     },
     created(){
-        this.loadEmails()
-
+        this.loadEmails();
+        eventBus.$on('send-filter', filter => {return this.setFilter(filter)})
+    },
+    destroyed(){
+            eventBus.$off('send-filter', (filter) => {
+            this.filterBy = filter
+            console.log(this.filterBy,'from bus');
+        })
     },
     components: {
         asideNav,
--- js/email/email-pages/email-details.cmp.js
+++ js/email/email-pages/email-details.cmp.js
@@ -1,12 +1,13 @@
 import asideNav from '../email-cmps/aside-nav.cmp.js'
 import {emailService} from '../services/email-storage-service.js'
+import { eventBus } from '../../services/event-bus-service.js'
 
 export default {
     template: `
     <section class="email">
-        <aside-nav />
+        <aside-nav @filter="setFilter" />
         <ul v-if="mail"> 
-            <li>{{mail.email}} </li>
+            <li>{{mail.email}}  <span @click="removeMail">Delete</span></li>
             <li>{{mail.name}} - {{mail.sentAt}} </li>
             <li>{{mail.subject}} </li>
             <li><pre>{{mail.body}}</pre> </li>
@@ -19,14 +20,21 @@ export default {
         }
     },
     methods: {
-
+        removeMail(){
+            emailService.remove(this.mail.id);
+            this.$router.push('/email')
+        },
+        setFilter(filter){
+            this.$router.push('/email/').catch(() => {});
+            eventBus.$emit('send-filter', filter)
+            
+        }
     },
     components: {
         asideNav
     },
     created() {
         const id = this.$route.params.id
-        console.log(id);
         emailService.getById(id)
             .then(mail => this.mail = mail)
     }
