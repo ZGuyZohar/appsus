6e5c058eb4a3e57489752e60c241b0b7622fde60
erez2021 <hershko2013@gmail.com>
2021-02-27 21:39:35 +0200

    final

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -4,16 +4,20 @@ import appHeader from '../cmps/app-header.cmp.js'
 
 export default {
     name: 'home-page',
-    template:`
+    template: `
     <section class="home app-main">
-    <h1>Welcome to Appsus</h1>
-       
+    <h1 class="home-header">Welcome to Appsus</h1>
+    <div class="home-container">   
+   <router-link  style="text-decoration: none" to="/keep/keep-page">     <div class="home-card"><h1 class="home-title">Keep</h1></div></router-link>
+    <router-link  style="text-decoration: none" to="/email">   <div class="home-card"><h1>E-Mail</h1> </div></router-link>
+     <router-link  style="text-decoration: none" to="book">  <div class="home-card"><h1>Books</h1> </div></router-link>
+    
+       </div>
 <footer><p> &copy; Coffeerights 2021</p></footer>
     </section>
-    `
-,
-components:{
-    appHeader,
-   
-},
+    `,
+    components: {
+        appHeader,
+
+    },
 }
