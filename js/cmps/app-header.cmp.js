04fe487e820f19ba3ec45ec8d66462aec15ac7da
erez2021 <hershko2013@gmail.com>
2021-02-27 16:54:27 +0200

    ffd

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,14 +1,25 @@
 export default {
     template: `
      <header class="app-header">
-     <h2 class="logo">Appsus</h2>
+     
+     <router-link  style="text-decoration: none;" to="/"><h2 class="logo">Appsus</h2></router-link>
     <nav>
     <router-link class="nav-links" to="/keep/keep-page">Keep</router-link>
     <router-link class="nav-links" to="/email">E-Mail</router-link>
     <router-link class="nav-links" to="/miss-book">Books</router-link>
-    <router-link class="nav-links" to="/">Home</router-link>
+    <!-- <router-link class="nav-links" to="/">Home</router-link> -->
 </nav>
-
+<button class="open-menu" v-if="navOpen" @click="toggleMenu" >☰</button>
 </header>
-    `
+    `,
+    data(){ 
+        return{
+        navOpen: false
+    }
+    },
+    methods: {
+        toggleMenu() {
+        this.navOpen = !this.navOpen
+        }
+    }
 }
