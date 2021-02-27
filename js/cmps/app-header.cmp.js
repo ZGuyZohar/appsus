8b5deb9962ab5ce5bed73c90a43da35b8a212331
erez2021 <hershko2013@gmail.com>
2021-02-27 19:00:24 +0200

    fdf

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,7 +1,7 @@
 export default {
     template: `
      <header class="app-header">
-     
+     <div @click.self="closeNav" :class="showNav" class="keep-responsive-container">
      <router-link  style="text-decoration: none;" to="/"><h2 class="logo">Appsus</h2></router-link>
     <nav :class="setClassName" >
     <router-link class="nav-links open-menu" to="/keep/keep-page">Keep</router-link>
@@ -10,22 +10,29 @@ export default {
    
 </nav>
 <button class="menu-btn"  @click="toggleMenu"  hidden>☰</button>
+</div>
 </header>
     `,
     data(){ 
         return{
-        navOpen: false
+        navOpen: null
     }
     },
     methods: {
         toggleMenu() {
         this.navOpen = !this.navOpen
     
+        },
+        closeNav() {
+            this.navOpen  =false
         }
     },
     computed: {
         setClassName() {
             if (this.navOpen) return 'open-menu'
+        },
+        showNav(){
+            return {'show-nav' :  !this.navOpen}
         }
     }
 }
